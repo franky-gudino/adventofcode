014b0f147da8b0fd74e8c9c16b78fc977c9e2e84
Franky Gudino <franky.gudino@headspace.com>
2025-12-11 17:05:40 -0800

    day 5 part 2

--- 2025/src/days/day_5.rs
+++ 2025/src/days/day_5.rs
@@ -1,12 +1,42 @@
 pub fn solve(contents: String) {
     let (id_ranges, ids) = parse_contents(contents);
-    let parsed_id_ranges: Vec<(u64, u64)> = id_ranges.iter().map(parse_id_range).collect();
+    let mut parsed_id_ranges: Vec<(u64, u64)> = id_ranges.iter().map(parse_id_range).collect();
     let parsed_ids: Vec<u64> = ids.iter().map(|id| id.parse().unwrap()).collect();
 
-    let fresh_ingredients: u64 = get_fresh_ingredients(&parsed_id_ranges, &parsed_ids);
+    let fresh_ingredients = get_fresh_ingredients(&parsed_id_ranges, &parsed_ids);
+    let all_fresh_ingredients = get_all_fresh_ingredients(&mut parsed_id_ranges);
 
     println!("Day 5:\n-----------------");
     println!("[Part 1] Fresh ingredients: {fresh_ingredients}");
+    println!("[Part 2] All fresh ingredients: {all_fresh_ingredients}");
+}
+
+fn get_all_fresh_ingredients(id_ranges: &mut Vec<(u64, u64)>) -> u64 {
+    if id_ranges.is_empty() {
+        return 0;
+    }
+
+    id_ranges.sort_by_key(|range| range.0);
+
+    let mut compressed_ranges: Vec<(u64, u64)> = Vec::new();
+    let mut curr_range = id_ranges[0];
+
+    for (l, r) in id_ranges.iter().skip(1) {
+        if *l <= curr_range.1 {
+            curr_range.1 = curr_range.1.max(*r);
+        } else {
+            compressed_ranges.push(curr_range);
+            curr_range = (*l, *r);
+        }
+    }
+
+    compressed_ranges.push(curr_range);
+
+    let total_ids = compressed_ranges
+        .iter()
+        .fold(0, |acc, (l, r)| acc + (r - l + 1));
+
+    total_ids
 }
 
 fn get_fresh_ingredients(id_ranges: &Vec<(u64, u64)>, ids: &Vec<u64>) -> u64 {
